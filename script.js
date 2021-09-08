051672cfe72fb9105a6d261af635795c063d634d
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2021-09-08 23:38:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ for(let i=0;i<9;i++)
     let cell=document.createElement('div');
     cell.style.border="1px solid black";
     cell.id=`box${i}`;
-    cell.innerText="V";
+    cell.innerText=":)";
     b[`box${i}`]=cell.innerText;
     cell.style.color="white";
     cell.addEventListener('click',()=>{
-        if (cell.innerText=="V"){
+        if (cell.innerText==":)"){
         cell.innerText=player;
         cell.style.color="black";
         b[`box${i}`]=cell.innerText;
         if (checkBoard())
         {
             alert(`${player} won`);
-            
+
         } 
         player=player=='X'?'O':'X';
 
@@ -30,21 +30,21 @@ for(let i=0;i<9;i++)
 }
 function checkBoard()
 {
-    if(b['box0']==b['box1']&&b['box1']==b['box2']&&b['box1']!='V')
+    if(b['box0']==b['box1']&&b['box1']==b['box2']&&b['box1']!=':)')
         return true;
-    else if (b['box3']==b['box4']&&b['box4']==b['box5']&&b['box4']!='V')
+    else if (b['box3']==b['box4']&&b['box4']==b['box5']&&b['box4']!=':)')
         return true;
-    else if (b['box6']==b['box7']&&b['box7']==b['box8']&&b['box8']!='V')
+    else if (b['box6']==b['box7']&&b['box7']==b['box8']&&b['box8']!=':)')
         return true;
-    else if (b['box0']==b['box3']&&b['box3']==b['box6']&&b['box0']!='V')
+    else if (b['box0']==b['box3']&&b['box3']==b['box6']&&b['box0']!=':)')
         return true;
-    else if (b['box1']==b['box4']&&b['box4']==b['box7']&&b['box7']!='V')
+    else if (b['box1']==b['box4']&&b['box4']==b['box7']&&b['box7']!=':)')
         return true;
-    else if (b['box2']==b['box5']&&b['box5']==b['box8']&&b['box5']!='V')
+    else if (b['box2']==b['box5']&&b['box5']==b['box8']&&b['box5']!=':)')
         return true;
-    else if (b['box0']==b['box4']&&b['box4']==b['box8']&&b['box4']!='V')
+    else if (b['box0']==b['box4']&&b['box4']==b['box8']&&b['box4']!=':)')
         return true;
-    else if (b['box2']==b['box4']&&b['box4']==b['box6']&&b['box4']!='V')
+    else if (b['box2']==b['box4']&&b['box4']==b['box6']&&b['box4']!=':)')
         return true;
     return false;
 
